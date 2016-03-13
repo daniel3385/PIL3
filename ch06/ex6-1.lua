2cf8df4e5e3a0d6293ff21dc74b6b8ec646ad063
daniel <daniel@debian-daniel>
2016-03-13 18:43:07 -0300

    ex 1

--- ch06/ex6-1.lua
+++ ch06/ex6-1.lua
@@ -2,4 +2,15 @@
 
 integral = function(f)
 	return function(x, y)
+		return (y-x)*f((x+y)/2)
+		end
+end
+
+--test
+d = function (x)
+	return x+2*x
+end
+c = integral(d)
+print(c(1, 2))
+print(c(3, 4))
 		
