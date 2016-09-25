276fba428e1d1301a73e5b6e99c788601e3839c1
daniel <daniel.cabral88@gmail.com>
2016-09-25 10:52:32 -0300

    3 done

---
+++ 01_GettingStarted/comment.lua
@@ -0,0 +1,13 @@
+--[[
+    Programming languages tha use -- to comment, from wikipedia:
+    Ada
+    AppleScript
+    Eiffel
+    Euphoria
+    Haskell
+    Lua 
+    SGML
+    SQL
+    VHDL
+--]]
+
