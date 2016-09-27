93c625079f28bdf8d92cf389205ac94e3f3d6a86
daniel <daniel.cabral88@gmail.com>
2016-09-27 20:01:18 -0300

    ex5-01

--- 05_Functions/concat.lua
+++ 05_Functions/concat.lua
@@ -1,11 +1,11 @@
 -- input strings then output them concatenated
 
 function concatenated(...)
-	local output = ""
-	for k, v in pairs{...} do
-		output = output .. v
-	end
-	return output
+    local output = ""
+    for k, v in pairs{...} do
+        output = output .. v
+    end
+    return output
 end
 
 print(concatenated("daniel","cabral","silveira","bla","bla2"))
--- ch05/ex5-3.lua
+++
@@ -1,9 +0,0 @@
--- input number output all but the first
-
-function remove_first(...)
-	local t = table.pack(...)
-	table.remove(t, 1)
-	return table.unpack(t)
-end
-
-print (remove_first(1,3,5,7,9,11,13,15,17))
