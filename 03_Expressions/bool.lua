5124e80be58044169bbc3e5181ce300253e70b3a
daniel <daniel.cabral88@gmail.com>
2017-03-04 17:57:52 -0300

    fixed bool.lua

--- 03_Expressions/bool.lua
+++ 03_Expressions/bool.lua
@@ -1,10 +1,10 @@
 -- verify boolen whithout use type()...
 
-function verifyBool(boll)
-    return (boll == true)
+function verifyBool(bool)
+    local result = false
+    if (bool == true) or (bool == false) then
+        result = true
+    end
+    return result
 end
 
-local a = true
-local b = false
-print(a)
-print(b)
